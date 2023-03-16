631ee362478d35217e8e13c24411fbffe108cc1e
mugishasam123 <mugishasamuel42@gmail.com>
2023-03-16 21:36:19 +0200

    Add valid tests

---
+++ tests/test_valid.py
@@ -0,0 +1,23 @@
+import unittest
+from scripts.modules.check_valid import valid
+
+class TestTruthy(unittest.TestCase):
+
+    def test_valid(self):
+        m = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
+        
+        self.assertTrue(valid(m, 0, 2, 4)) # Test valid value
+        self.assertFalse(valid(m, 0, 2, 5)) # Test invalid value
+
+if __name__ == '__main__':
+    unittest.main()
