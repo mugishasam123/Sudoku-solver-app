88c6939c213ec044ae6a48168dde95f33c91d32d
mugishasam123 <mugishasamuel42@gmail.com>
2023-03-16 21:23:34 +0200

    Add check valid number function

---
+++ scripts/modules/check_valid.py
@@ -0,0 +1,13 @@
+def valid(m, i, j, val):
+	for it in range(9):
+		if m[i][it]== val:
+			return False
+		if m[it][j]== val:
+			return False
+	it = i//3
+	jt = j//3
+	for i in range(it * 3, it * 3 + 3):
+		for j in range (jt * 3, jt * 3 + 3):
+			if m[i][j]== val:
+				return False
+	return True
